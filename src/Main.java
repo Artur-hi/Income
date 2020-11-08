06f39e10047b2c59b6bf6457a9832cd88021ad7f
Artur <artur.hi_rul@mail.ru>
2020-11-08 19:59:00 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 1000;
+        int rate =3;
+        int income = (1000/100) * 3;
+
+        System.out.println(income);
+
+
+    }
+}
